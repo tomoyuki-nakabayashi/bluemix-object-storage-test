61cf0e6dbd766144836a79ec8843b646cf926cf4
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2017-06-18 17:23:43 +0900

    Add put

--- src/swift-post.py
+++ src/swift-post.py
@@ -36,6 +36,7 @@ def swift_auth():
 def swift_get(response):
     token = response.headers['X-Subject-Token']
     endpoint = response.json()['token']['catalog'][7]['endpoints'][3]['url']
+    endpoint = endpoint + '/test'
     headers = {'X-Auth-Token': token}
 
     response = requests.get(endpoint, headers=headers)
@@ -43,6 +44,22 @@ def swift_get(response):
     print (response.headers)
     print (response.text)
 
+def swift_put(response):
+    token = response.headers['X-Subject-Token']
+    endpoint = response.json()['token']['catalog'][7]['endpoints'][3]['url']
+    endpoint = endpoint + '/test/python-test.txt'
+    headers = {'X-Auth-Token': token, 'Content-Type': 'text/plain'}
+    params = {'file': 'python-test.txt'}
+
+    with open('./python-test.txt') as textFile:
+        mydata = textFile.read()
+        files = {'python-test.txt': mydata}
+        response = requests.put(endpoint, headers=headers, files=files)
+        print (response.status_code)
+        print (response.headers)
+        print (response.text)
+
 if __name__ == "__main__":
     auth_resp = swift_auth()
     swift_get(auth_resp)
+    swift_put(auth_resp)

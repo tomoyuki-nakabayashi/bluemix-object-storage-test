9ad3ae3516d983c5d2cdfcf3a9751c5ca2741aa6
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2017-06-18 20:21:42 +0900

    fix #4 succeeded in uploading text file

--- src/swift-post.py
+++ src/swift-post.py
@@ -26,11 +26,6 @@ def swift_auth():
     }'''
 
     response = requests.post(url, data=data, headers=headers)
-    #print (response.json())
-    #print (response.headers)
-    #print (response.headers['X-Subject-Token'])
-    #print (response.json()['token']['catalog'][0]['endpoints'][0]['url'])
-
     return response
 
 def swift_get(response):
@@ -48,13 +43,11 @@ def swift_put(response):
     token = response.headers['X-Subject-Token']
     endpoint = response.json()['token']['catalog'][7]['endpoints'][3]['url']
     endpoint = endpoint + '/test/python-test.txt'
-    headers = {'X-Auth-Token': token, 'Content-Type': 'text/plain'}
-    params = {'file': 'python-test.txt'}
+    headers = {'X-Auth-Token': token, 'Content-Type': 'text/html; charset=UTF-8'}
 
-    with open('./python-test.txt') as textFile:
-        mydata = textFile.read()
-        files = {'python-test.txt': mydata}
-        response = requests.put(endpoint, headers=headers, files=files)
+    with open('./python-test.txt') as myfile:
+        mydata = myfile.read()
+        response = requests.put(endpoint, headers=headers, data=mydata)
         print (response.status_code)
         print (response.headers)
         print (response.text)

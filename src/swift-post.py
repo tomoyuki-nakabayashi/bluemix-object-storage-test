1ac8400816424a314383c29b0647475daa6ef8b6
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2017-06-18 11:31:49 +0900

    Succeed to upload file. But, file is not correct....

--- src/swift-post.py
+++ src/swift-post.py
@@ -1,40 +1,48 @@
 # coding: utf-8
 import requests
 
-url = 'https://lon-identity.open.softlayer.com/v3/auth/tokens'
-headers={'Content-Type': 'application/json'}
-data = '''{
-    "auth": {
-        "identity": {
-            "methods": [
-                "password"
-            ],
-            "password": {
-                "user": {
-                    "id": "",
-                    "password": ""
+def swift_auth():
+    url = 'https://lon-identity.open.softlayer.com/v3/auth/tokens'
+    headers={'Content-Type': 'application/json'}
+    data = '''{
+        "auth": {
+            "identity": {
+                "methods": [
+                    "password"
+                ],
+                "password": {
+                    "user": {
+                        "id": "b3c26c9b0a3f47a584211908c643812a",
+                        "password": "L2yLC~BZ[H/Mzd~1"
+                    }
+                }
+            },
+            "scope": {
+                "project": {
+                    "id": "c281af0f80fc4410a02dfe9167a3f284"
                 }
             }
-        },
-        "scope": {
-            "project": {
-                "id": ""
-            }
         }
-    }
-}'''
+    }'''
 
-response = requests.post(url, data=data, headers=headers)
-#print (response.json())
-#print (response.headers)
-#print (response.headers['X-Subject-Token'])
-#print (response.json()['token']['catalog'][0]['endpoints'][0]['url'])
+    response = requests.post(url, data=data, headers=headers)
+    #print (response.json())
+    #print (response.headers)
+    #print (response.headers['X-Subject-Token'])
+    #print (response.json()['token']['catalog'][0]['endpoints'][0]['url'])
 
-token = response.headers['X-Subject-Token']
-endpoint = response.json()['token']['catalog'][7]['endpoints'][3]['url']
-headers = {'X-Auth-Token': token}
+    return response
 
-response = requests.get(endpoint, headers=headers)
-print (response.status_code)
-print (response.headers)
-print (response.text)
+def swift_get(response):
+    token = response.headers['X-Subject-Token']
+    endpoint = response.json()['token']['catalog'][7]['endpoints'][3]['url']
+    headers = {'X-Auth-Token': token}
+
+    response = requests.get(endpoint, headers=headers)
+    print (response.status_code)
+    print (response.headers)
+    print (response.text)
+
+if __name__ == "__main__":
+    auth_resp = swift_auth()
+    swift_get(auth_resp)
